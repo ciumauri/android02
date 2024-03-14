0318bebdf8bf1e99299cd9def2506ede29c876f9
Mauricio Oliveira <ciumauri@gmail.com>
2024-03-14 20:08:12 -0300

    Finalizando lógica do projeto, Exercicio 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLimpar.setOnClickListener(this);
+        binding.btnExibir.setOnClickListener(this);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.btnLimpar) {
+            binding.edtNome.setText("");
+            binding.rdgSexo.clearCheck();
+            binding.edtEmail.setText("");
+            binding.edtTelefone.setText("");
+            binding.chkMusica.setChecked(false);
+            binding.chkCinema.setChecked(false);
+            binding.chkEsporte.setChecked(false);
+            binding.chkGastronomia.setChecked(false);
+            binding.swtNotificacoes.setChecked(false);
+        } else if (v.getId() == R.id.btnExibir) {
+            if (binding.lblDados.getVisibility() == View.VISIBLE) {
+                binding.lblDados.setVisibility(View.INVISIBLE);
+            } else {
+                binding.lblDados.setVisibility(View.VISIBLE);
+                binding.txtNome.setText("Nome: " + binding.edtNome.getText().toString());
+                binding.txtSexo.setText("Sexo: " + (binding.rdbMasculino.isChecked() ? "Masculino" : "Feminino"));
+                binding.txtEmail.setText("E-mail: " + binding.edtEmail.getText().toString());
+                binding.txtTelefone.setText("Telefone: " + binding.edtTelefone.getText().toString());
+                binding.txtPreferencias.setText("Interesses: " +
+                        (binding.chkMusica.isChecked() ? "Música " : "") +
+                        (binding.chkCinema.isChecked() ? "Cinema " : "") +
+                        (binding.chkEsporte.isChecked() ? "Esporte " : "") +
+                        (binding.chkGastronomia.isChecked() ? "Gastronomia " : ""));
+                binding.txtNotificacoes.setText("Notificações: " + (binding.swtNotificacoes.isChecked() ? "Ativadas" : "Desativadas"));
+            }
+
+        }
+
     }
 }
